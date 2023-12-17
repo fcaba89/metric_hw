9a63da0bb462b1705f6c88dbc20521c04d4dac01
Franky Caba <34558715+fcaba89@users.noreply.github.com>
2023-12-17 18:26:00 -0500

    Update metric_hw.sh

--- metric_hw.sh
+++ metric_hw.sh
@@ -1,32 +1,57 @@
 #!/bin/bash
 
-log() {
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" >> metric_hw.txt
-}
+# Thresholds
+CPU_THRESHOLD=80 # in percentage
+MEMORY_THRESHOLD=80 # in percentage
 
+# Function to check CPU usage
 check_cpu() {
-  cpu_threshold=90
-  cpu_usage=$(top -l 1 | awk '/CPU usage:/ {print int($3)}')
-
-  if [ "$cpu_usage" -gt "$cpu_threshold" ]; then
-    log "High CPU usage: $cpu_usage%"
-elisf [ "$cpu_usage" =0]
-  else
-    log "CPU usage within normal range: $cpu_usage%"
-  fi
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | awk -F. '{print $1}')
+        echo "CPU Usage: $cpu_usage%" >> metrics.txt
+    if [ $cpu_usage -gt $CPU_THRESHOLD ]; then
+        echo "CPU usage is above threshold! ($CPU_THRESHOLD%)" >> metrics.txt
+        # You can add actions here like sending an alert/notification
+    fi
 }
 
-# Function to check disk space
+# Function to check memory usage
+check_memory() {
+    memory_usage=$(free | awk '/Mem/{printf("%.2f"), $3/$2*100}')
+    echo "Memory Usage: $memory_usage%" >> metrics.txt
+    if (( $(echo "$memory_usage > $MEMORY_THRESHOLD" | bc -l) )); then
+        echo "Memory usage is above threshold! ($MEMORY_THRESHOLD%)" >> metrics.txt
+        # You can add actions here like sending an alert/notification
+    fi
+}
+# Function to check disk usage
 check_disk() {
-  disk_threshold=90
-  disk_usage=$(df -H | awk '$NF=="/" {print int($5)}')
-
-  if [ "$disk_usage" -gt "$disk_threshold" ]; then
-    log "High disk usage: $disk_usage%"
-  else
-    log "Disk usage within normal range: $disk_usage%"
-  fi
+    echo "Disk Usage:" >> metrics.txt
+    df -h
 }
 
-check_cpu
-check_disk
+# Function to check the status of a specific service
+check_service_status() {
+    echo "Service Status:"
+    service_name="ssh" # Change this to the service you want to monitor
+    systemctl status $service_name
+}
+
+# Function to monitor logs
+monitor_logs() {
+    echo "Last 10 lines of syslog:"
+    tail -n 10 /var/log/syslog # Change this to the log file you want to monitor
+}
+
+# Main function to execute monitoring tasks
+main() {
+    echo "Monitoring System Metrics and Services"
+    echo "-------------------------------------"
+    check_cpu
+    check_memory
+     check_disk
+    check_service_status
+    monitor_logs
+}
+
+# Execute main function
+main
